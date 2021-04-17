34a94fb1a6a75d5057e6fb414f410d5f0ff7d7f8
Stefan Arseneau <sarsene1@jhu.edu>
2021-04-16 23:04:24 -0400

    added piano timbre

--- synth/synth.py
+++ synth/synth.py
@@ -5,10 +5,13 @@ from scipy.io import wavfile
 def key_freq(n):
     return (2**(1 / 12))**(n - 49) * 440 # Hz
 
+# Converts the note and octave into a frequency
 def note_freq(octave, note):
+    # The note progression of most octaves
     notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'] 
 
     if octave == 0:
+        # 0th octave has only these 3
         notes = ['A', 'A#', 'B']
 
         if note in notes:
@@ -19,6 +22,7 @@ def note_freq(octave, note):
             return 0
 
     elif octave == 8:
+        # One note in the 8th octave
         notes = ['C']
 
         if note in notes:
@@ -29,6 +33,7 @@ def note_freq(octave, note):
             return 0
 
     else:
+        # Else is a normal octave
         if note in notes:
             n = 12 * (octave - 1) + notes.index(note) + 4
             freq = key_freq(n)
@@ -37,6 +42,7 @@ def note_freq(octave, note):
             print('Err: Note not in octave!')
             return 0
 
+# This function is stolen, bit of a black box to me
 def get_adsr_weights(frequency, duration, length, decay, sustain_level, sample_rate=44100):
 
     assert abs(sum(length)-1) < 1e-8
@@ -73,6 +79,7 @@ def get_adsr_weights(frequency, duration, length, decay, sustain_level, sample_r
         weights = np.concatenate((weights, weights[-1]-weights[-1]/tail*np.arange(tail)))
     return weights
 
+# Gets the pure frequency of a note. No timbre
 def get_note(octave, note, duration = 0, beats = 0, bpm = 0, sample_rate=44100, amplitude=4096, save = False):
     if duration == 0:
         duration = beats * (60 / bpm)
@@ -93,23 +100,34 @@ def get_note(octave, note, duration = 0, beats = 0, bpm = 0, sample_rate=44100, 
     for i in range(len(freqs)):
         wave += amps[i]*np.sin(2*np.pi*freqs[i]*t)
 
-    if save:
-        wavfile.write(('' + str(note) + str(octave) + '.wav'), rate=44100, data=wave.astype(np.int16))
-
     return wave, frequency
 
-def piano(octave, note, duration, lens = [0.05, 0.25, 0.55, 0.15], save = False):
-    note, f = get_note(octave, note, duration, save = save)
+# Adds the timbre of a piano to the pure frequency
+def piano(octave, note, duration, lens = [0.05, 0.25, 0.55, 0.15], sustain_level = 0.1, decay = [0.085, 0.02, 0.005, 0.1], save = False):
+    wav, f = get_note(octave, note, duration, save = save)
 
-    weights = get_adsr_weights(f, duration, lens, 
-    return note
+    weights = get_adsr_weights(f, duration, lens, decay = decay, sustain_level = sustain_level)
+    
+    dat = wav * weights
+    dat = dat * (4096/np.max(dat))
 
-octave = int(input('Octave: '))
-note = str(input('Note: '))
+    if save:
+        wavfile.write(('' + str(note) + str(octave) + '.wav'), rate=44100, data=dat.astype(np.int16))
+
+    return dat
+
+octave1 = int(input('Octave: '))
+note1 = str(input('Note: '))
+
+octave2 = int(input('Octave: '))
+note2 = str(input('Note: '))
+
 duration = int(input('Duration: '))
 
-w1 = piano(octave, note, duration, save = True)
-w2 = get_note(4, 'G', 4, save = True)
+
+
+w1 = piano(octave1, note1, duration, save = True)
+w2 = piano(octave2, note2, duration, save = True)
 
 fin = w1 + w2
 
