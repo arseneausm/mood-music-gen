c65c3fea645356c1cbd9219a1d9a68c524b5cec8
matthewpolson <82674940+matthewpolson@users.noreply.github.com>
2021-04-17 14:48:25 -0400

    implementing Progression class (very buggy)

--- chords/progression hell.py
+++ chords/progression hell.py
@@ -1,3 +1,5 @@
+import random
+
 nameToNum = {'C': 0,
              'C#': 1,
              'Db': 1,
@@ -45,24 +47,34 @@ numToName = {0: 'C',
 class Note:
     def __init__(self, noteName):
         self.noteName = noteName
-        self.noteNumber = Note.toChromaticDegree(noteName)
+        self.noteNumber = Note.to_chromatic_degree(noteName)
 
     def __str__(self):
         return self.noteName
 
     @classmethod
-    def toChromaticDegree(cls, noteName):
+    def to_chromatic_degree(cls, noteName):
         return nameToNum[noteName]
 
-    def sharp(self):
+    def sharpen(self):
         self.noteNumber = (self.noteNumber + 1) % 12
         notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
         self.noteName = notes[self.noteNumber]
 
-    def flat(self):
+    def sharp(self):
+        i = (self.noteNumber + 1) % 12
+        notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+        return notes[i]
+
+    def flatten(self):
         self.noteNumber = (self.noteNumber - 1) % 12
         notes = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
         self.noteName = notes[self.noteNumber]
+
+    def flat(self):
+        i = (self.noteNumber - 1) % 12
+        notes = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+        return notes[i]
 
     @classmethod
     def get_interval(cls, note1, note2):
@@ -94,19 +106,19 @@ minorModes = [modes[5],  # mixolydian
 
 class Chord:
 
-    def changeDegree(self, degree, sharp):
+    def change_degree(self, degree, sharp):
         noteIntervals = self.get_intervals_in_chord()
         if degree in noteIntervals:
             i = noteIntervals.index(degree)
             if sharp:
-                self.notes[i].sharp()
+                self.notes[i].sharpen()
                 return True
             else:
-                self.notes[i].flat()
+                self.notes[i].flatten()
                 return True
 
     # ['C', 'D', 'E', 'F', 'G', 'A', 'B']
-    def __init__(self, modeNum, rootDegree, numNotes):
+    def __init__(self, modeNum, numNotes, rootDegree):
         # rootDegree indexed at 1 (for the sake of ii VI I etc)
         self.mode = modes[modeNum]
         noteDegree = rootDegree - 1
@@ -114,17 +126,27 @@ class Chord:
         self.notes = []
         nextNote = self.mode[noteDegree]
         for i in range(numNotes):
-            self.notes.append(nextNote)
-            noteDegree = (noteDegree + 2) % 7
             nextNote = self.mode[noteDegree]
-        if rootDegree == 7:
-            self.changeDegree(7, False)
+            if nextNote not in self.notes:
+                self.notes.append(nextNote)
+            else:
+                return
+            noteDegree = (noteDegree + 2) % 7
+
+    def copy_of(self, template):
+        self.mode = template.mode
+        self.root = template.root
+        self.notes = template.notes
 
     def __str__(self):
-        chordString = ''
+        voicing = ''
         for note in self.notes:
-            chordString = chordString + str(note) + ' '
-        return chordString
+            voicing = voicing + str(note) + ' '
+        return voicing
+
+    def add_note(self, noteToAdd):
+        if noteToAdd not in self.notes:
+            self.notes.append(noteToAdd)
 
     def get_intervals_in_chord(self):
         intervals = []
@@ -132,85 +154,121 @@ class Chord:
             intervals.append(Note.get_interval(self.root, note))
         return intervals
 
-    def transposeUp(self, steps):
+    def transpose_up(self, steps):
         newChord = []
         for note in self.notes:
             for i in range(steps):
-                newChord.append(note.sharp())
+                newChord.append(note.sharpen())
         return newChord
 
-    def transposeDown(self, steps):
+    def transpose_down(self, steps):
         newChord = []
         for note in self.notes:
             for i in range(steps):
-                newChord.append(note.flat())
+                newChord.append(note.flatten())
         return newChord
 
-    def minor(self):
-        return self.changeDegree(4, False)
-        # if 11 in noteIntervals:
-        #   i = noteIntervals.index(11)
-        #  self.notes[i].flat()
+    def major_minor_switch(self):
+        if self.change_degree(3, True):
+            self.change_degree(10, True)
+            return
+        if self.change_degree(4, False):
+            self.change_degree(11, False)
+            return
 
-    def major(self):
-        return self.changeDegree(3, True)
-        # if 10 in noteIntervals:
-        #   i = noteIntervals.index(11)
-        #   self.notes[i].sharp()
+    def to_sus2(self):
+        self.change_degree(4, False)
+        self.change_degree(3, False)
+        # self.notes[1] = numToName[(root.to_chromatic_degree + 2)]
 
-    def sus2(self):
-        self.changeDegree(4, False)
-        self.changeDegree(3, False)
-        # self.notes[1] = numToName[(root.toChromaticDegree + 2)]
+    def to_sus4(self):
+        self.change_degree(4, True)
+        self.change_degree(3, True)
 
-    def sus4(self):
-        self.changeDegree(4, True)
-        self.changeDegree(3, True)
+
+moodMod1Dict = {"happy": random.choice([majorModes[0], majorModes[1]]),
+                }
+moodMod2Dict = {"sleepy": [8,  # numChords
+                           3  # degreeStart
+                           ]
+                }
+moodMod3Dict = {"spicy": [[4, 5, 6],  # numNotesRange
+                          0.75,  # chance for accidentals
+                          5,  # weight for mode degrees
+                          [2, 5, 6, 7]  # mode degrees to weight
+                          ],
+                "salt": [[2, 3],  # numNotesRange
+                         0,  # chance for accidentals
+                         0,  # weight for mode degrees
+                         []  # mode degrees to weight
+                         ],
+                }
+
+
+class Progression:
+
+    def __init__(self,
+                 moodMod1,
+                 moodMod2,
+                 moodMod3):
+        self.chordSequence = []
+        self.modeUsed = moodMod1Dict[moodMod1]
+        self.numNotes = None
+        self.numNotesRange = [3]
+        weightedModeDegrees = [1, 1, 2, 3, 4, 5, 6, 7]
+        self.degreeSequence = []
+
+        self.numChords = moodMod2Dict[moodMod2[0]]
+        self.degreeSequence.append(moodMod2Dict[moodMod2[1]])
+
+        self.numNotesRange = moodMod3Dict[moodMod3[0]]
+        self.chanceForAccidentals = moodMod3Dict[moodMod3[1]]
+        self.weight = moodMod3Dict[moodMod3[2]]
+        self.degreesToWeight = moodMod3Dict[moodMod3[3]]
+        for i in range(self.weight):
+            for d in range(self.degreesToWeight):
+                weightedModeDegrees.append(moodMod3Dict[moodMod3[3][d]])
+
+        # add new chord
+        for i in range(self.numChords):
+            self.chords.append(self.gen_chord(self.degreeSequence[i]))
+            self.degreeSequence.append(random.choice(weightedModeDegrees))
+
+    def __str__(self):
+        for chord in self.chordSequence:
+            print str(chord)
+
+    def gen_chord(self, degree):
+        self.numNotes = random.choice(self.numNotesRange)
+        chord = Chord(self.modeUsed, self.numNotes, degree)
+        if random.random() <= self.chanceForAccidentals:
+            chord.major_minor_switch()
+        return chord
 
     # TODO
-    # def addSeventh(self):
-    #    self.notes.append()
+    # chords = an array of chords
+    # change order of notes based on surrounding chords
+    def voice_leading(self, chords):
+        self.chords = []
+        for i in (range(len(chords)) - 1):
+            self.chords.append(chords[i].voiced(i + 1))
+        pass
 
 
-c = Chord(1, 1, 5)  # C E G
-# chordIII = chordI.transposeUp(4).minor() # E G B
-# chordIV = chordI.transposeUp(5).major() # F A C
-# chordV = chordI.transposeUp(7).major() # G B D
-# chordVI = chordI.transposeUp(9).minor() # A C E
+# c1 = Chord(1, 4, 2)  # D F A C
+# c2 = Chord(1, 4, 5)  # G B D F
+# c3 = Chord(1, 4, 1)  # C E G B
+# print c1
+# print c2
+# print c3
 
+# rootChord = Chord(1, 1, 1)
+# rootChord.copy_of(c3)
+# print rootChord
+# tempChord = Chord(1, 3, 1)
 
-# notesList = [Note('C'), Note('Eb'), Note('G')]
-# notesList[1].sharp()
-# Cmaj7 = Chord(notesList)
+moodMod1 = "happy"
+moodMod2 = "sleepy"
+moodMod3 = "spicy"
 
-# print Cmaj7
-# print Cmaj7.get_intervals_in_chord()
-# Cmaj7.minor()
-# print Cmaj7
-
-# I chord
-
-print c
-# ii chord
-c.transposeUp(2)
-c.minor()
-print c
-# iii chord
-c.transposeUp(2)
-print c
-# IV chord
-c.transposeUp(1)
-c.major()
-print c
-# V chord
-c.transposeUp(2)
-print c
-# vi chord
-c.transposeUp(2)
-c.minor()
-print c
-# vii
-c.transposeUp(2)
-c.minor()
-print c
-
+testProgression = Progression(moodMod1, moodMod2, moodMod3)
